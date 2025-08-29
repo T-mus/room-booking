307c567c180ba0814df611dbe29d687c673667a1
Raymond Lead <adasta355@gmail.com>
2025-08-29 18:35:18 +0300

    Remove unnecessary file

--- test/common.utils.ts
+++
@@ -1,4 +0,0 @@
-export const getConnectionString = () =>
-    `${process.env.MONGO_PROTOCOL}` +
-    `${process.env.MONGO_LOGIN}:${process.env.MONGO_PASSWORD}` +
-    `@${process.env.MONGO_HOST}/${process.env.MONGO_DB}`
